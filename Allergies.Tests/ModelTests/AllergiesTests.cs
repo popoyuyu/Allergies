090ae71e9597b1130491f8fa14d64482f8ccc8d1
Jeff Lai <pjefflai@gmail.com>
2021-12-07 15:53:29 -0800

    second test multiple items pass
    Co-Authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- Allergies.Tests/ModelTests/AllergiesTests.cs
+++ Allergies.Tests/ModelTests/AllergiesTests.cs
@@ -12,5 +12,12 @@ namespace Allergies.Tests
       Allergens.decodeAllergens(1);
       Assert.AreEqual("eggs", Allergens.decodeAllergens(1)[0]);
     }
+    [TestMethod]
+    public void IsAllergen_selectingMultipleCorrectItems_True()
+    {
+      Assert.AreEqual("cats 2018", Allergens.decodeAllergens(131)[0]);
+      Assert.AreEqual("peanuts", Allergens.decodeAllergens(131)[1]);
+      Assert.AreEqual("eggs", Allergens.decodeAllergens(131)[2]);
+    }
   }
 }
